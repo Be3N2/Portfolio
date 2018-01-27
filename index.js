0bde130aad2590080061d22ab3338b9626ff3a76
Be3N2 <2017WakefieldB@gmail.com>
2018-01-27 23:05:14 +0000

    server made changes

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get("/", function (request, response) {
+  response.sendFile('public/index.html');
+});
+
+app.get("/data", function (request, response) {
+  response.send({"data": 10});
+});
+
 app.listen(3000, function() {
   console.log("server running on port 3000");
 });
