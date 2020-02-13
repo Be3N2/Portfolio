f2fa228cb34a667a64c62e344c37039573faf103
Be3N2 <2017WakefieldB@gmail.com>
2020-02-13 12:40:51 -0500

    Buttons to remove and add graphed paths

--- public/core.js
+++ public/core.js
@@ -147,14 +147,19 @@ $("#failTimeSelector").change(function() {
               .attr("class","chart");
 
   	drawAxis(chart, loadedData[selectedNum]["Brake"].length, 0, 100);
-  	drawData(chart, loadedData[selectedNum]["Brake"], 0, 100, "steelblue");
-  	drawData(chart, loadedData[selectedNum]["Speed"], 0, 100, "red");
-  	drawData(chart, loadedData[selectedNum]["Steering"], -1, 1, "darkgreen");
+  	drawData(chart, loadedData[selectedNum]["Brake"], 0, 100, "brakePath", "steelblue");
+  	drawData(chart, loadedData[selectedNum]["Speed"], 0, 100, "speedPath", "red");
+  	drawData(chart, loadedData[selectedNum]["Steering"], -1, 1, "steeringPath", "darkgreen");
+  	//activate buttons
+  	$("#controls").toggle(true);
 
   	drawSteeringAxis(chart, loadedData[selectedNum]["Steering"], -1, 1); 
 
   	if (loadedData[selectedNum]["SteeringData"].fullApprox) {
-  		drawData(chart, loadedData[selectedNum]["SteeringData"].fullApprox, -1, 1, "green");
+  		drawData(chart, loadedData[selectedNum]["SteeringData"].fullApprox, -1, 1, "steeringApproxPath", "lightgreen");
+  		$("#approxToggle").toggle(true);
+  	} else {
+  		$("#approxToggle").toggle(false);
   	}
 });
 
@@ -206,28 +211,30 @@ function drawSteeringAxis(chart, data, min, max) {
 		      .attr("fill", "none")
 		      .attr("stroke", "lightgrey")
 		      .attr("stroke-width", 1.5)
-		      .attr("id", "steering")
+		      .attr("id", "steeringPathBounds")
 		      .attr("d", d3.line()
 		        .x(function(d,i) {return xScale(i) })
 		        .y(function(d) { return yScale(0.10) })
 		 	  ); 
+
 	chart.append("path")
 		      .datum(data)
 		      .attr("fill", "none")
 		      .attr("stroke", "lightgrey")
 		      .attr("stroke-width", 1.5)
-		      .attr("id", "steering")
+		      .attr("id", "steeringPathBounds")
 		      .attr("d", d3.line()
 		        .x(function(d,i) {return xScale(i) })
 		        .y(function(d) { return yScale(-0.10) })
-		        ); 
+		      ); 
     
     chart.append('g')
 				.attr('transform', 'translate('+(w-padding)+',0)')
+				.attr("id", "steeringScalePath")
 				.call(yAxis);
 }
 
-function drawData(chart, data, min, max, color) {
+function drawData(chart, data, min, max, id, color) {
 
 	var yScale = d3.scaleLinear()
 		.domain([min, max])
@@ -242,131 +249,29 @@ function drawData(chart, data, min, max, color) {
 	  .attr("fill", "none")
 	  .attr("stroke", color)
 	  .attr("stroke-width", 1.5)
-	  .attr("id", "speed")
+	  .attr("id", id)
 	  .attr("d", d3.line()
 	    .x(function(d,i) {return xScale(i) })
 	    .y(function(d) { return yScale(d) })
 	);
 }
 
-/*
-d3.request("./assets/Data3Test.csv")
-		.mimeType("text/csv")
-		.response(function (xhr) {
-			var data = d3.csvParse(xhr.responseText);
-			var formattedArr = [];
-			data.forEach((d) => {
-				var formatted = [+d["Fail Time"], +d.Speed, +d.Brake, +d.Gas, +d.Steering, +d["X-Position"], +d["Z-Position"], +d["Cosine Curves"]];
-				formattedArr.push(formatted);
-			});
-			return formattedArr;
-		})
-		.get(function(data) {
+$("#speedToggle").on("click", function() {
+	var opacity = d3.select("#speedPath").style("opacity");
+	d3.selectAll("#speedPath").transition().style("opacity", opacity == 1 ? 0:1)
+});
 
-			var maxBrake = d3.max(data, datum => datum[2]);
+$("#brakeToggle").on("click", function() {
+	var opacity = d3.select("#brakePath").style("opacity");
+	d3.selectAll("#brakePath").transition().style("opacity", opacity == 1 ? 0:1)
+});
 
-			var brakeYScale = d3.scaleLinear()
-				.domain([0, maxBrake])
-				.range([h-padding, padding]);
+$("#steeringToggle").on("click", function() {
+	var opacity = d3.select("#steeringPath").style("opacity");
+	d3.selectAll("#steeringPath").transition().style("opacity", opacity == 1 ? 0:1);;	
+});
 
-			chart.append("path")
-		      .datum(data)
-		      .attr("fill", "none")
-		      .attr("stroke", "red")
-		      .attr("stroke-width", 1.5)
-		      .attr("id", "brake")
-		      .attr("d", d3.line()
-		        .x(function(d,i) {return xScale(i) })
-		        .y(function(d,i) {return brakeYScale(d[2])})
-		    );
-
-		    //cosine is always last column in processed data
-		    var cosLocation = data[0].length - 1;
-		    var maxCosCurve = d3.max(data, datum => datum[cosLocation]);
-		    var CosCurveScale = d3.scaleLinear()
-		    	.domain([0, maxCosCurve])
-		    	.range([h - padding, padding])
-		    chart.append("path")
-		      .datum(data)
-		      .attr("fill", "none")
-		      .attr("stroke", "yellow")
-		      .attr("stroke-width", 1.5)
-		      .attr("id", "cosCurve")
-		      .attr("d", d3.line()
-		        .x(function(d,i) {return xScale(i) })
-		        .y(function(d,i) {return CosCurveScale(d[cosLocation])})
-		    );
-
-		    var maxGas = d3.max(data, datum => datum[3]);
-
-			var gasYScale = d3.scaleLinear()
-				.domain([0, maxGas])
-				.range([h-padding, padding]);
-		      
-		    chart.append("path")
-		      .datum(data)
-		      .attr("fill", "none")
-		      .attr("stroke", "black")
-		      .attr("stroke-width", 1.5)
-		      .attr("id", "gas")
-		      .attr("d", d3.line()
-		        .x(function(d,i) {return xScale(i) })
-		        .y(function(d) { return gasYScale(d[3]) })
-		    ); 
-
-		    drawSteeringChart(data);
-
-		    var ySteeringScale = d3.scaleLinear()
-				   .domain([-1,1])
-				   .range([h-padding, padding])
-
-			var xSteeringScale = d3.scaleLinear()
-			       .domain([0, data.length])
-			       .range([padding, w-padding]);
-			var ySteeringAxis = d3.axisRight()
-				  .scale(ySteeringScale);
-
-			chart.append('g')
-				.attr('transform', 'translate('+ (w - padding)+',0)')
-				.attr("id", "steeringOverlay")
-				.call(ySteeringAxis);
-
-			chart.append("path")
-		      .datum(data)
-		      .attr("fill", "none")
-		      .attr("stroke", "green")
-		      .attr("stroke-width", 1.5)
-		      .attr("id", "steeringOverlay")
-		      .attr("d", d3.line()
-		        .x(function(d,i) {return xSteeringScale(i) })
-		        .y(function(d) { return ySteeringScale(d[4]) })
-		 	  ); 
-		});
-
-function toggleSpeed() {
-	var opacity = d3.select("#speed").style("opacity");
-	d3.selectAll("#speed").transition().style("opacity", opacity == 1 ? 0:1)
-}
-
-function toggleBrake() {
-	var opacity = d3.select("#brake").style("opacity");
-	d3.selectAll("#brake").transition().style("opacity", opacity == 1 ? 0:1)
-}
-
-function toggleGas() {
-	var opacity = d3.select("#gas").style("opacity");
-	d3.selectAll("#gas").transition().style("opacity", opacity == 1 ? 0:1)
-}
-
-function toggleSteering() {
-	var opacity = d3.select("#steeringOverlay").style("opacity");
-	d3.selectAll("#steeringOverlay").transition().style("opacity", opacity == 1 ? 0:1);;
-}
-
-function toggleCos() {
-	var opacity = d3.select("#cosCurve").style("opacity");
-	d3.selectAll("#cosCurve").transition().style("opacity", opacity == 1 ? 0:1)
-}
-
-*/
-
+$("#approxToggle").on("click", function() {	
+	var opacity = d3.select("#steeringApproxPath").style("opacity");
+	d3.selectAll("#steeringApproxPath").transition().style("opacity", opacity == 1 ? 0:1)	
+});
