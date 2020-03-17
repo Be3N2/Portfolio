bb02184c3913e1972c7c50a266abce4bac278da4
Be3N2 <2017WakefieldB@gmail.com>
2020-03-17 11:41:20 -0400

    Live server test branch

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ app.get('/', (request, response) => {
   response.sendFile('public/index.html');
 });
 
-app.post('/formData', upload.any(),(request, response)=> {
+app.post('/test', (request, response) => {
+	console.log('request body', request.body);
+	response.sendStatus(200);
+});
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+// POST /login gets urlencoded bodies
+app.post('/form', urlencodedParser, function (request, response) {
+ if(request.body) { 
+	console.log(request.body);
+	for (var prop in request.body) {
+ 	 console.log(request.body[prop]);
+	}
+}
+ if(request.query) console.log('request query' + request.query);
+  response.sendStatus(200);
+});
+
+
+app.post('/formData', upload.none(),(request, response)=> {
 	const formData = request.body;
+	if (formData.book1) {
+		console.log("book1", book1);
+	} else {
+		console.log("Empty formdata obj no book1");
+	}
 	console.log('form data', formData);
 	response.sendStatus(200);
 });
@@ -21,4 +45,4 @@ const server = app.listen(8080, () => {
   const port = server.address().port;
 
   console.log(`Example app listening at http://${host}:${port}`);
-});
+x});
